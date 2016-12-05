4350043aa2950b13617d7c099a35ffe906c34e4d
Tsinat Zeree <cheru87@gmail.com>
2016-12-05 15:27:03 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    './js/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
